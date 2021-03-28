8a19b8200a80990be6a04e216444ced7aa198f1d
Juankamm <juankamm94@gmail.com>
2021-03-28 20:53:51 +0200

    Adding Checkbuttons and Delete button

--- Campos de text.py
+++ Campos de text.py
@@ -28,12 +28,18 @@ tabControl.pack(expand = 1, fill = "both")
 
 # Tab How to use
 
+
+
 title_label = Label(HowToUseTab, text = "Welcome to PCF Creator", font=("Consolas", 24))
-title_label.pack()
+title_label.grid()
 
 # Tab submit.html
 
-Label(submitTab, text = "Choose the fields that you want to delete:").pack(anchor=NW)
+introFrame = Frame(submitTab)
+introFrame.grid()
+
+
+Label(introFrame, text = "Choose the fields that you want to delete:").grid(sticky = W)
 
 #Variables for the Checkbuttons
 
@@ -41,15 +47,43 @@ submit_title = StringVar(submitTab, "0")
 submit_dateRequested = StringVar(submitTab, "0")
 submit_targetAudience = StringVar(submitTab, "0")
 submit_quoteRequired = StringVar(submitTab, "0")
+submit_ReferenceProjectNumber = StringVar(submitTab, "0")
+submit_ServicesList = StringVar(submitTab, "0")
+submit_TranslateFrom = StringVar(submitTab, "0")
+submit_TranslateInto = StringVar(submitTab, "0")
+submit_TargetDate = StringVar(submitTab, "0")
+submit_PurchaseOrder = StringVar(submitTab, "0")
 submit_submittedBy = StringVar(submitTab, "0")
+submit_SendBackOriginal = StringVar(submitTab, "0")
 
 # submit.html checkbuttons 
 
-Checkbutton(submitTab, text = "Title", variable = submit_title, onvalue = 1, offvalue = 0).pack(anchor=NW)
-Checkbutton(submitTab, text = "Date requested", variable = submit_dateRequested, onvalue = 1, offvalue = 0).pack(anchor=NW)
-Checkbutton(submitTab, text = "Target audience", variable = submit_targetAudience, onvalue = 1, offvalue = 0).pack(anchor=NW)
-Checkbutton(submitTab, text = "Quote Required", variable = submit_quoteRequired, onvalue = 1, offvalue = 0).pack(anchor=NW)
-Checkbutton(submitTab, text = "Submitted By", variable = submit_submittedBy, onvalue = 1, offvalue = 0).pack(anchor=NW)
+
+FieldsFrame = Frame(submitTab)
+FieldsFrame.grid()
+
+
+Checkbutton(FieldsFrame, text = "Title", variable = submit_title, onvalue = 1, offvalue = 0).grid(column = 1, row = 1, sticky = W, pady = 10)
+Checkbutton(FieldsFrame, text = "Date requested", variable = submit_dateRequested, onvalue = 1, offvalue = 0).grid(column = 2, row = 1, sticky = W,pady = 10)
+Checkbutton(FieldsFrame, text = "Target audience", variable = submit_targetAudience, onvalue = 1, offvalue = 0).grid(column = 3, row = 1, sticky = W,pady = 10)
+Checkbutton(FieldsFrame, text = "Quote required", variable = submit_quoteRequired, onvalue = 1, offvalue = 0).grid(column = 1, row = 2, sticky = W,pady = 10)
+Checkbutton(FieldsFrame, text = "Reference project number", variable = submit_ReferenceProjectNumber, onvalue = 1, offvalue = 0).grid(column = 2, row = 2, sticky = W,pady = 10)
+Checkbutton(FieldsFrame, text = "Services list", variable = submit_ServicesList, onvalue = 1, offvalue = 0).grid(column = 3, row = 2, sticky = W,pady = 10)
+Checkbutton(FieldsFrame, text = "Translate from", variable = submit_TranslateFrom, onvalue = 1, offvalue = 0).grid(column = 1, row = 3, sticky = W,pady = 10)
+Checkbutton(FieldsFrame, text = "Translate into", variable = submit_TranslateInto, onvalue = 1, offvalue = 0).grid(column = 2, row = 3, sticky = W,pady = 10)
+Checkbutton(FieldsFrame, text = "Target date", variable = submit_TargetDate, onvalue = 1, offvalue = 0).grid(column = 3, row = 3, sticky = W,pady = 10)
+Checkbutton(FieldsFrame, text = "Purchase order", variable = submit_PurchaseOrder, onvalue = 1, offvalue = 0).grid(column = 1, row = 4, sticky = W,pady = 10)
+Checkbutton(FieldsFrame, text = "Submitted By", variable = submit_submittedBy, onvalue = 1, offvalue = 0).grid(column = 2, row = 4, sticky = W,pady = 10)
+Checkbutton(FieldsFrame, text = "Send back original", variable = submit_SendBackOriginal, onvalue = 1, offvalue = 0).grid(column = 3, row = 4, sticky = W,pady = 10)
+
+
+#Delete button
+
+DeleteBtnFrame = Frame(submitTab)
+DeleteBtnFrame.grid()
+
+DeleteBtn = Button(DeleteBtnFrame, text = "Delete fields")
+DeleteBtn.grid()
 
 
 
